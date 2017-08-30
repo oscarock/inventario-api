dc646baf0eb0c72c2479412b1267c1797bec7277
Your Name <you@example.com>
2017-08-30 16:40:59 -0500

    Correcciones API

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,8 +1,12 @@
-class ArticlesController < ApiController
-  before_action :require_login
-  
-  # This is protected by API token
-  def index
-    render json: { articles: 'index articulos'}
-  end
+module Api
+  module V1
+		class ArticlesController < ApiController
+		  before_action :require_login
+		  
+		  # This is protected by API token
+		  def index
+		    render json: { articles: 'index articulos'}
+		  end
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      get 'articles/index'	
-      scope :format => true, :constraints => { :format => 'json' } do
+      # get 'articles/index'	
+      # scope :format => true, :constraints => { :format => 'json' } do
         post   "/login"       => "sessions#create"
         delete "/logout"      => "sessions#destroy"
-      end
-   end
+      # end
+    end  
+  end
 end
