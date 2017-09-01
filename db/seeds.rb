c91f24e3e21b3cb25e173f4f937bee484e465d0b
Your Name <you@example.com>
2017-09-01 09:49:37 -0500

    Crea seed para la base de datos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 User.destroy_all
 User.create(name: 'prueba1', email: 'prueba1@prueba1.com', password: '123456', profile_id: 1)
 User.create(name: 'prueba2', email: 'prueba2@prueba2.com', password: '123456', profile_id: 2)
@@ -16,3 +17,12 @@ Profile.create(description: 'perfil de usuario', name: 'Usuario')
 MovementType.destroy_all
 MovementType.create(name: 'Entrada')
 MovementType.create(name: 'Salida')
+
+Article.destroy_all
+Article.create(code: 1213, description: 'camisa', user_id: 1)
+Article.create(code: 1214, description: 'esferos', user_id: 2)
+
+Movement.destroy_all
+Movement.create(movement_type_id: 3, article_id: 1, amount: 30)
+Movement.create(movement_type_id: 4, article_id: 1, amount: 10)
+Movement.create(movement_type_id: 3, article_id: 2, amount: 15)
