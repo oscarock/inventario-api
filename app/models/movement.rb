417db21f2619b65802b10c8371168825ce16ed65
Your Name <you@example.com>
2017-09-01 09:50:33 -0500

    Ajusta modelo

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,10 +1,10 @@
 class Movement < ApplicationRecord
 	#validations
 	validates :movement_type_id, presence: true
-	validates :amount, presence: true
 	validates :article_id, presence: true
+	validates :amount, presence: true
 
 	#relations
-  	belongs_to :articles, optional: true
   	belongs_to :movement_type, optional: true
+  	belongs_to :article, optional: true
 end
